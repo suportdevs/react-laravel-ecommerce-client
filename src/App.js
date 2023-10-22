6ef1f297c8bfe90d08fee4e843681fae3eb1f60e
suportdevs <suportdevs@gmail.com>
2023-10-22 23:32:16 +0600

    updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
 import Product from "./pages/Product/Product";
 import ProductList from "./pages/ProductList/ProductList";
+import Register from "./pages/Register/Register";
 
 function App() {
   return (
     <>
       {/* <Home /> */}
       {/* <ProductList /> */}
-      <Product />
+      {/* <Product /> */}
+      {/* <Register /> */}
+      <Login />
     </>
   );
 }
