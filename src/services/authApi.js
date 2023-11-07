edcef6b157b4db24d7e94bdb820dafb35e298207
suportdevs <suportdevs@gmail.com>
2023-11-07 23:45:11 +0600

    working

--- src/services/authApi.js
+++ src/services/authApi.js
@@ -4,11 +4,6 @@ export const authApi = createApi({
     reducerPath: 'authApi',
     baseQuery: fetchBaseQuery({baseUrl: process.env.REACT_APP_BACKEND_BASE_URL}),
     endpoints: (builder) => ({
-        // csrf: builder.query({
-        //     query: () => ({
-        //         url: '/sanctum/csft-token',
-        //     }),
-        // }),
         register: builder.mutation({
             query: ({data}) => ({
                 url: '/register',
